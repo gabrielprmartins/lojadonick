4c2ae18eb5f3a3e17fddb939997a9b7150ca9c7f
gabriuda <gabrielpr.martins@gmail.com>
2021-01-14 21:32:52 -0300

    correções no código javascript

--- js/script.js
+++ js/script.js
@@ -3,18 +3,16 @@ for (let i = 0; i < 100; i++) {
     precos[i] = window.document.querySelectorAll('#preco')[i].innerHTML;
 }
 
-function precosMembro() {
-    for (let i = 0; i < 100; i++) {
-        window.document.querySelectorAll('#preco')[i].innerHTML = `${precos[i].toLocaleString('pt-BR', { minimumFractionDigits: 0})}`;
-    }
-}
-
 function aplicarPromo(porcentagem) {
     for (let i = 0; i < 100; i++) {
         window.document.querySelectorAll('#preco')[i].innerHTML = `${precos[i].toLocaleString('pt-BR', { minimumFractionDigits: 0})}`;
 
         let precos2 = window.document.querySelectorAll('#preco')[i].innerHTML.replace('.','');
 
-        window.document.querySelectorAll('#preco')[i].innerHTML = `<span class="preco-anterior">${precos[i]}</span>${(precos2 - ((porcentagem / 100) * precos2)).toLocaleString('pt-BR', { minimumFractionDigits: 0})}`;
+        if (porcentagem < 1) {
+            window.document.querySelectorAll('#preco')[i].innerHTML = `${(precos2 - ((porcentagem / 100) * precos2)).toLocaleString('pt-BR', { minimumFractionDigits: 0})}`;
+        } else {
+            window.document.querySelectorAll('#preco')[i].innerHTML = `<span class="preco-anterior">${precos[i]}</span>${(precos2 - ((porcentagem / 100) * precos2)).toLocaleString('pt-BR', { minimumFractionDigits: 0})}`;
+        }
     }
 }
